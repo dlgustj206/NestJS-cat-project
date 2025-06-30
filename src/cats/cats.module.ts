fb4c40b23f865c21079c3a86e2031d81c6a1bd86
이현서 <dlgustj206@naver.com>
2025-06-30 23:19:42 +0900

    feat: Repository 패턴 도입 및 서비스 레이어 분리

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -3,11 +3,12 @@ import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Cat, CatSchema } from './cats.schema';
+import { CatsRepository } from './cats.repository';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
   controllers: [CatsController],
-  providers: [CatsService], // 서비스(또는 로직)를 Nest에 등록 -> DI 가능
+  providers: [CatsService, CatsRepository], // 서비스(또는 로직)를 Nest에 등록 -> DI 가능
   exports: [CatsService],
 })
 export class CatsModule {}
---
+++ src/cats/cats.repository.ts
@@ -0,0 +1,18 @@
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Cat } from './cats.schema';
+import { HttpException } from '@nestjs/common';
+import { CatRequestDto } from './dto/cats.request.dto';
+
+export class CatsRepository {
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+
+  async existByEmail(email: string): Promise<boolean> {
+    const result = await this.catModel.exists({ email });
+    return result !== null;
+  }
+
+  async create(cat: CatRequestDto): Promise<Cat> {
+    return await this.catModel.create(cat);
+  }
+}
