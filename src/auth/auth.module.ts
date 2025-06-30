04dc0b1e93ed14c8a8dcb7d0e55cfe8f3906bc9f
이현서 <dlgustj206@naver.com>
2025-07-01 00:33:07 +0900

    feat: JWT 모듈 설정 및 로그인 서비스 구현, 순환 참조 해결

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { forwardRef, Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt/jwt.strategy';
+import { CatsModule } from 'src/cats/cats.module';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt', session: false }),
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '1y' },
+    }),
+
+    forwardRef(() => CatsModule), // CatsModule에 export된 걸 사용할 수 있음
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,14 +1,18 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Cat, CatSchema } from './cats.schema';
 import { CatsRepository } from './cats.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }]),
+    forwardRef(() => AuthModule),
+  ],
   controllers: [CatsController],
   providers: [CatsService, CatsRepository], // 서비스(또는 로직)를 Nest에 등록 -> DI 가능
-  exports: [CatsService],
+  exports: [CatsService, CatsRepository],
 })
 export class CatsModule {}
